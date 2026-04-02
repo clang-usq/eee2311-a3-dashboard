6e964470919286db09d64a73eb35cbf49c892b06
Christopher Lang <u1077170@umail.usq.edu.au>
2026-04-03 09:41:54 +1000

    updated js/css for CYS 240x320

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # eee2311-a3-dashboard
 Garden Monitoring System HMI/Dashboard
+Simple dashboard example to represent measurements received from the API.
--- static/main.js
+++ static/main.js
@@ -3,10 +3,13 @@ console.log('Loaded Garden Monitoring System');
 
 //can get this from a fetch request to the API
 const weatherData = {
-  humidity: 64,
-  windSpeed: 12,
-  windDirection: "NE",
-  rain: 10
+	humidity: 64,
+	windSpeed: 12,
+	windDirection: "NE",
+	rain: 10,
+	//epoch time - then convert in function to local timestamp
+	lastUpdate: 1775172151,
+	//add additional if needed
 };
 
 /**
@@ -22,15 +25,34 @@ const weatherData = {
  * @param {weatherData} data
  */
 function updateDashboard(data) {
-  document.getElementById("humidity").textContent = data.humidity;
-  document.getElementById("windSpeed").textContent = data.windSpeed;
-  document.getElementById("windDirection").textContent = data.windDirection;
-  document.getElementById("rain").textContent = data.rain;
+	document.getElementById("humidity").textContent = data.humidity;
+	document.getElementById("windSpeed").textContent = data.windSpeed;
+	document.getElementById("windDirection").textContent = data.windDirection;
+	document.getElementById("rain").textContent = data.rain;
+	document.getElementById("lastUpdate").textContent = data.lastUpdate;
 }
 
 
 // fetch request to API endpoint
+async function requestWeather(){
+	let response = await fetch(
+		"http://192.168.1.50/api/v1/sensor",
+	);
+	//log an error if status is not 200. 
+	if(!response.ok) {
+		throw new Error(`Error: ${response.status}`);
+	}
+	//else log the response and parse as json.
+	const data = await response.json();
+	console.log(data);
+}
 
+//pass the json response here to format data to send to updateDashboard
+function parseJson(){
+	console.log('Parsing received data');
 
+	//parse data here
+
+}
 
 updateDashboard(weatherData);
